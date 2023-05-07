3ba0dcc24cdef1ae4c6f466e328120fa9d739970
GoveRaven <daniilevgen79@gmail.com>
2023-05-07 12:24:46 +0500

    Обновил квадрат

--- Болотище/Квадрат/js/border.js
+++ Болотище/Квадрат/js/border.js
@@ -1,14 +1,46 @@
-let square = document.querySelector(".square");
-let border = document.querySelector(".border");
+let borderRadius = document.querySelector(".borderRadius");
+let borderColor = document.querySelector(".borderColor");
+let borderWidth = document.querySelector(".borderWidth");
+let borderStyle = document.querySelector(".border-style");
 
-function changeBorder(value) {
+function changeBorderRaduis(value) {
   square.style.borderRadius = `${value}%`;
-  if (border.value === "") {
+  if (borderRadius.value === "") {
     square.style.borderRadius = "0%";
   }
 }
 
-border.addEventListener("keyup", () => {
-  let num = border.value;
-  changeBorder(num);
+borderRadius.addEventListener("input", () => {
+  let num = borderRadius.value;
+  changeBorderRaduis(num);
 });
+
+function changeBorderColor(color) {
+  square.style.borderColor = color
+}
+
+borderColor.addEventListener('keyup', () => {
+  let value = borderColor.value
+  changeBorderColor(value)
+})
+
+function changeBorderWidth(value) {
+  square.style.borderWidth = `${value}px`
+  if (value === '') {
+    square.style.borderWidth = `1px`
+  }
+}
+
+borderWidth.addEventListener('input', () => {
+  let value = borderWidth.value
+  changeBorderWidth(value)
+})
+
+function changeBorderStyle(style) {
+  square.style.borderStyle = style
+}
+
+borderStyle.addEventListener('change', () => {
+  let style = borderStyle.value
+  changeBorderStyle(style)
+})
