8fb95623ae1f1e5a87f033c7bdec489546732a9f
Даня <daniilevgen79@gmail.com>
2023-10-29 16:29:12 +0300

    что-то

--- Болотище/Тренировка/script.js
+++ Болотище/Тренировка/script.js
@@ -1,39 +1,73 @@
-function checkCoupon(currentDate, expirationDate) {
-  const months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  const currDataArr = currentDate.split(" ");
-  const expirDataArr = expirationDate.split(" ");
-  let currDataMonth = currDataArr[0];
-  let expirDataMonth = expirDataArr[0];
+const cats = document.querySelectorAll(".cat");
+const zone1 = document.getElementById("zone1");
+const zone2 = document.getElementById("zone2");
 
-  currDataMonth = months.indexOf(currDataMonth)
-  expirDataMonth = months.indexOf(expirDataMonth)
+cats.forEach((cat) => {
+  cat.draggable = true;
+  cat.addEventListener("dragstart", drag);
+})
 
-  const currData = `${currDataArr[2]}-${currDataMonth}-${currDataArr[1]}`
-  const expirData = `${expirDataArr[2]}-${expirDataMonth}-${expirDataArr[1]}`
 
-  const data1 = new Date(currentDate)
-  const data2 = new Date(expirationDate)
 
-  console.log(data1)
-  console.log(data2)
-
-  if (currDataArr[2] <= expirDataArr[2]) {
-    return true;
-  }
-  return false;
+function drag(event) {
+  event.dataTransfer.setData('id', event.target.id)
+  console.log(event.dataTransfer)
 }
 
-console.log(checkCoupon("November 8, 2013", "November 5, 2014"));
+zone1.ondragover = zone2.ondragover = (event) => false
+zone1.ondrop = zone2.ondrop = drop
+
+function drop(event) {
+  event.preventDefault()
+
+  const movedEl = event.dataTransfer.getData('id')
+
+  this.append(document.getElementById(movedEl))
+}
+
+
+
+// const list = document.getElementById("list");
+// const lis = list.querySelectorAll(".list_item");
+
+// lis.forEach((el) => (el.draggable = true));
+
+// list.addEventListener("dragstart", (event) => {
+//   event.target.classList.add("selected");
+// });
+
+// list.addEventListener("dragend", (event) => {
+//   event.target.classList.remove("selected");
+// });
+
+// list.addEventListener("dragover", (event) => {
+//   event.preventDefault();
+
+//   const activedEl = list.querySelector(".selected");
+//   const currentEl = event.target;
+//   const isMoveable =
+//     activedEl !== currentEl && currentEl.classList.contains("list_item");
+
+//   if (!isMoveable) return;
+
+//   const nextEl = getNextElement(event.clientY, currentEl);
+
+//   if (
+//     (nextEl && activedEl === nextEl.previousElementSibling) ||
+//     activedEl === nextEl
+//   ) {
+//     return;
+//   }
+
+//   nextEl.before(activedEl);
+
+//   // list.insertBefore(activedEl, nextEl)
+// });
+
+// function getNextElement(cursorPosition, currentEl) {
+//   const currentElCoords = currentEl.getBoundingClientRect();
+//   const currentElCenter = currentElCoords.y + currentElCoords.height / 2;
+//   const nextEl =
+//     cursorPosition < currentElCenter ? currentEl : currentEl.nextElementSibling;
+//   return nextEl;
+// }
