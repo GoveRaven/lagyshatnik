96bf39aeb3b49fce7833880508c802c8b754fab7
Даня <daniilevgen79@gmail.com>
2023-11-16 23:06:42 +0300

    some 2.2

--- Болотище/Connect_with_GitHub/script.js
+++ Болотище/Connect_with_GitHub/script.js
@@ -1,9 +1,14 @@
+const GitHubButton = document.getElementById("GitHubButton");
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
 import {
   getAuth,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   onAuthStateChanged,
+  GithubAuthProvider,
+  signInWithPopup,
+  signOut,
 } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
 
 // TODO: Replace the following with your app's Firebase project configuration
@@ -18,29 +23,32 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
 
-signInWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed in
-    const user = userCredential.user;
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-  });
+// Создание аккаунта через почту
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed up
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   });
 
+// signInWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed in
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//   });
+
+//Проверка авторизации пользователя
 onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
@@ -52,3 +60,45 @@ onAuthStateChanged(auth, (user) => {
     // ...
   }
 });
+
+// Вход в систему с помощью всплывающего окна
+const provider = new GithubAuthProvider();
+
+GitHubButton.addEventListener("click", signInWithGitHub);
+
+function signInWithGitHub() {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      const credential = GithubAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+      console.log(token)
+      console.log(user)
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GithubAuthProvider.credentialFromError(error);
+      // ...
+      console.log(errorCode)
+    });
+    console.log('work')
+}
+
+//Выход из системы
+signOut(auth)
+  .then(() => {
+    // Sign-out successful.
+  })
+  .catch((error) => {
+    // An error happened.
+  });
